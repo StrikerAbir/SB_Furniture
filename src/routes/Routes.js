5f817bd8352761591a7090cca5e9eb2fdb711224
Abir Hasan <cse.170201063@gmail.com>
2022-11-25 05:48:51 +0600

    second layout created

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,9 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import SecondLayout from "../layouts/SecondLayout";
 import BLogs from "../pages/Blogs/Blogs";
 import Categories from "../pages/Categories/Categories";
 import CategoryProducts from "../pages/Categories/CategoryCard/CategoryProducts/CategoryProducts";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
   {
@@ -29,5 +32,19 @@ export const router = createBrowserRouter([
           fetch(`http://localhost:1000/categories/products?cat_id=${params.id}`),
       },
     ],
-  },
+    },
+    {
+        path: "/",
+        element: <SecondLayout></SecondLayout>,
+        children: [
+            {
+                path: "/login",
+                element: <Login></Login>
+            },
+            {
+                path: "/signUp",
+                element: <SignUp></SignUp>
+            },
+        ]
+    }
 ]);
