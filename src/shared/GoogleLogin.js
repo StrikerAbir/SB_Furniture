2f0b56faf81df74d81e25e4cd70cbe69028ba23e
Abir Hasan <cse.170201063@gmail.com>
2022-11-25 08:10:47 +0600

    post user in db while login with google

--- src/shared/GoogleLogin.js
+++ src/shared/GoogleLogin.js
@@ -1,22 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import toast from "react-hot-toast";
 import { useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../authProvider/AuthProvider";
-
-
+import useToken from "../hooks/useToken";
 
 const GoogleLogin = () => {
   const { googleProviderLogin } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
   const from = location?.state?.from?.pathname || "/";
+  const [loginUserEmail, setLoginUserEmail] = useState("");
+  const [token] = useToken(loginUserEmail);
+
+  if (token) {
+    navigate(from, { replace: true });
+  }
 
   const handleGoogleLogin = () => {
     googleProviderLogin()
       .then((result) => {
         const user = result.user;
         console.log(user);
-        navigate(from, { replace: true });
+        saveUser(user.displayName, user.email);
         toast.success("Successfully login..");
       })
       .catch((err) => {
@@ -24,8 +29,24 @@ const GoogleLogin = () => {
         toast.error(err.message);
       });
   };
+  const saveUser = (name, email) => {
+    const user = { name, email, user_type: "Buyer" };
+    fetch("http://localhost:1000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLoginUserEmail(email);
+      });
+  };
   return (
-    <button onClick={handleGoogleLogin} className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+    <button onClick={handleGoogleLogin} className="btn btn-outline w-full">
+      CONTINUE WITH GOOGLE
+    </button>
   );
 };
 
