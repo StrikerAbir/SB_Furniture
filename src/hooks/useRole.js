4d90a13b2d9779ca0adae1262f73ee7cda5a00df
Abir Hasan <cse.170201063@gmail.com>
2022-11-26 23:35:33 +0600

    role hook done for user type

---
+++ src/hooks/useRole.js
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+const useRole = (email) => {
+
+  const [roles, setRole] = useState('');
+  const url = `http://localhost:1000/users/userType/${email}`;
+
+  const { data: role, refetch, } = useQuery({
+    queryKey: ["role", email],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setRole(data)
+      return data;
+    }
+  });
+  return [roles]
+}
+
+export default useRole;
